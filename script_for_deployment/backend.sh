f220c6ddbe9bc7ff7f0a744b26dd83dfb87b4aae
wqhuang-ustc <wqhuang.ustc@gmail.com>
2019-03-09 18:10:57 +0100

    add script for k8s deployment

---
+++ script_for_deployment/backend.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/ubuntu/Hello-world-deployment/k8s_yaml/ && pwd
+TAG=$1
+sed -i "s/hello_world_backend:.*/hello_world_backend:latest/g" backend_deployment.yaml
+kubectl replace -f backend_deployment.yaml  --record
+sleep 30
+sed -i "s/hello_world_backend:.*/hello_world_backend:${TAG}/g" backend_deployment.yaml
+kubectl replace -f backend_deployment.yaml  --record
+echo "Done"
---
+++ script_for_deployment/frontend.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/ubuntu/Hello-world-deployment/k8s_yaml/ && pwd
+TAG=$1
+sed -i "s/hello_world_frontend:.*/hello_world_frontend:latest/g" frontend_deployment.yaml
+kubectl replace -f frontend_deployment.yaml  --record
+sleep 30
+sed -i "s/hello_world_frontend:.*/hello_world_frontend:${TAG}/g" frontend_deployment.yaml
+kubectl replace -f frontend_deployment.yaml  --record
+echo "Done"
