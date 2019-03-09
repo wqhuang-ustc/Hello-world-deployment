e822276dd1a0cde4b40c87e622c7c645909e30e9
wqhuang-ustc <wqhuang.ustc@gmail.com>
2019-03-09 22:23:36 +0100

    remove --record

--- script_for_deployment/backend.sh
+++ script_for_deployment/backend.sh
@@ -2,8 +2,8 @@
 cd /home/ubuntu/Hello-world-deployment/k8s_yaml/ && pwd
 TAG=$1
 sed -i "s/hello_world_backend:.*/hello_world_backend:latest/g" backend_deployment.yaml
-kubectl replace -f backend_deployment.yaml  --record
+kubectl replace -f backend_deployment.yaml
 sleep 30
 sed -i "s/hello_world_backend:.*/hello_world_backend:${TAG}/g" backend_deployment.yaml
-kubectl replace -f backend_deployment.yaml  --record
+kubectl replace -f backend_deployment.yaml
 echo "Done"
